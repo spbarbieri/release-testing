439d239c69c1a6e103f7a02338e777c21df97ef6
spbarbieri <spbgamer@yahoo.com>
2025-05-14 11:52:19 -0400

    testblb

---
+++ dir/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script displays the current time, how long the system has been running, the number of active users, and the system load averages over the last 1, 5, and 15 minutes.
+
+uptime
