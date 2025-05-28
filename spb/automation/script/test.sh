9fe9a11492ce5c72dec27c225922d874653d764c
spbarbieri <spbgamer@yahoo.com>
2025-05-28 16:19:57 -0400

    This is a sample commit

---
+++ spb/automation/script/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find . -name "*.yaml" -delete
+
+# This command will recursively search for all files with the .yaml extension in the current directory and its subdirectories, and then delete them.
