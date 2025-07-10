4eb10e3d0d7b56684dad3286623ec1d52925044e
spbarbieri <spbgamer@yahoo.com>
2025-07-10 11:36:31 -0400

    Test release 300

---
+++ test300.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script runs the ls command with the -alh options
+# The -a option lists all files and directories, including hidden ones
+# The -l option formats the output in a long list format
+# The -h option formats the sizes of files and directories in a human-readable format
+
+ls -alh
