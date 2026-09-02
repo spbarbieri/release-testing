418643a382440cbf3b54151665753cda10dce9bc
spbarbieri <spbgamer@yahoo.com>
2026-09-02 10:21:47 -0400

    New script to echo hello world

---
+++ automation/release-test/hello-world-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+# ------------------------------------------------------------
+# Script: hello_world.sh
+# Description: Prints a friendly greeting to standard output.
+# ------------------------------------------------------------
+
+# Print the message
+echo "Hello World"
